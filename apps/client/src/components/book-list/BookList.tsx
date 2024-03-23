0b1420479c6c65e8406f09317d84480f9e6f5896
Stanescu Bogdan <bogdan.stanescu96@gmail.com>
2024-03-23 15:47:30 +0200

    feat: create BookList component

---
+++ apps/client/src/components/book-list/BookList.tsx
@@ -0,0 +1,15 @@
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import BookCard from '../book-card/BookCard';
+import { itemData } from './mockData';
+
+export default function BookList() {
+  return (
+    <ImageList sx={{ width: '100%', height: '100vh' }}>
+      <ImageListItem key="Subheader" cols={2}></ImageListItem>
+      {itemData.map((item) => (
+        <BookCard key={item.id} {...item} />
+      ))}
+    </ImageList>
+  );
+}
---
+++ apps/client/src/components/book-list/mockData.ts
@@ -0,0 +1,59 @@
+export const itemData = [
+  {
+    id: 1,
+    image: 'https://upload.wikimedia.org/wikipedia/en/d/d2/Baltagul.jpg',
+    title: 'Baltagul',
+    genre: 'Action',
+    author: 'Mihail Sadoveanu',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    id: 2,
+    image:
+      'https://m.media-amazon.com/images/I/91WFb-PpoNL._AC_UL640_QL65_T2F_.jpg',
+    title: 'The Pragmatic Programmer',
+    author: 'David Thomas',
+    genre: 'Programming',
+    description:
+      'The Pragmatic Programmer: Your Journey to Mastery, 20th Anniversary Edition (2nd Edition) (Pragmatic Programmers)',
+  },
+  {
+    id: 3,
+    image:
+      'https://m.media-amazon.com/images/I/81HqVRRwp3L._AC_UL640_QL65_T2F_.jpg',
+    title: 'Eloquent JavaScript',
+    author: 'Marijn Haverbeke',
+    genre: 'Programming',
+    description:
+      'Eloquent JavaScript, 3rd Edition: A Modern Introduction to Programming',
+  },
+  {
+    id: 4,
+    image:
+      'https://m.media-amazon.com/images/I/81OvszBEdhL._AC_UL640_QL65_T2F_.jpg',
+    title: 'Fluent Python',
+    author: 'Luciano Ramalho',
+    genre: 'Programming',
+    description: 'Fluent Python: Clear, Concise, and Effective Programming',
+  },
+  {
+    id: 5,
+    image:
+      'https://m.media-amazon.com/images/I/81QaVrv6p4L._AC_UL640_QL65_T2F_.jpg',
+    title: 'Learning Go',
+    author: 'Jon Bodner',
+    genre: 'Programming',
+    description:
+      'Learning Go: An Idiomatic Approach to Real-World Go Programming',
+  },
+  {
+    id: 6,
+    image:
+      'https://m.media-amazon.com/images/I/51E2055ZGUL._AC_UL640_QL65_T2F_.jpg',
+    title: 'Clean Code',
+    author: 'Robert C. Martin',
+    genre: 'Programming',
+    description: 'Clean Code: A Handbook of Agile Software Craftsmanship',
+  },
+];
