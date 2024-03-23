aa47d55885f11a25d9c5396a8903edfbb767b8d9
Stanescu Bogdan <bogdan.stanescu96@gmail.com>
2024-03-24 00:16:35 +0200

    chore: show content if user is authed

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,9 +1,16 @@
+import { Box } from '@mui/material';
+import { useAuth } from '@clerk/clerk-react';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
-import { Box } from '@mui/material';
 import { Routes } from '../Routes';
 
 const App = () => {
+  const { isLoaded } = useAuth();
+
+  if (!isLoaded) {
+    return null;
+  }
+
   return (
     <Box>
       <Header />
