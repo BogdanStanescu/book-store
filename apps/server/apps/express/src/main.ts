809e48039c49bdaddadc1bd435ee9609699b27e3
Stanescu Bogdan <bogdan.stanescu96@gmail.com>
2024-03-24 21:03:27 +0200

    chore: update book description and genre

--- apps/server/apps/express/src/main.ts
+++ apps/server/apps/express/src/main.ts
@@ -17,10 +17,10 @@ let books = [
     id: 1,
     image: 'https://upload.wikimedia.org/wikipedia/en/d/d2/Baltagul.jpg',
     title: 'Baltagul',
-    genre: 'Action',
+    genre: 'Romanian Literature',
     author: 'Mihail Sadoveanu',
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      '"Baltagul" is a social novel written by Mihail Sadoveanu and first published in book form in November 1930 by Editura Cartea Românească in Bucharest.',
   },
   {
     id: 2,
